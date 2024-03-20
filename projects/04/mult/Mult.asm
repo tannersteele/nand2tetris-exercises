0c63114c480254a6cff8b411f980cc442ef4684c
Tanner Steele <tannerjsteele@gmail.com>
2024-03-19 19:56:47 -0500

    ASM multiply tests for ch4

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,4 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// Optimization note: @R2 could be used as the sole output buffer, kept 'sum' separate for cleanliness
+// This implementation keeps all the registers in their original form, you can avoid temps by overwriting their values.
+
+    @R1
+    D=M         // Load value from R1 into D register
+    @i
+    M=D         // Set iterator register in R1's register
+    @R2
+    M=0         // Clear the R2 register
+    @sum
+    M=0         // Clear the sum register
+
+(LOOPBEGIN)
+    @i
+    D=M         // Load current iterator register value into data register to check if we're done iterating
+    @LOOPEND
+    D;JEQ
+    @R0
+    D=M         // Load R0 into current data register to calculate running 'sum'
+    @sum
+    M=D+M
+    @i          // Decrement iterator register by 1
+    M=M-1
+    @LOOPBEGIN
+    D;JMP
+
+(LOOPEND)
+    @sum
+    D=M 
+    @R2
+    M=D         // Set R2 register contents to running sum
+
+(END)
+    @END        // Loop forever to avoid encountering downstream instructions
+    0;JMP
