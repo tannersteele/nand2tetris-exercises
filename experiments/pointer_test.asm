86c2433cb25f6e1bae0f4705fc029e23ad509e3d
Tanner Steele <tannerjsteele@gmail.com>
2024-03-20 21:01:22 -0500

    hacky asm implementation of pointer logic

--- experiments/pointer_test.asm
+++ experiments/pointer_test.asm
@@ -1,33 +1,39 @@
-// Starting at base address R0, set first R1 words to -1
+// R0 = n
+// Occupy memory from RAM[1] -> RAM[n] where it's register value is the corresponding memory address
+// ie. RAM[1] = 1
 
-	@n
-	M=0
-	@R1
-	D=M
-	@x
-	M=D+1
+@R0
+D=M
+@n
+M=D
+@k
+M=0
+@counter
+M=1
 
-(LOOP)
-	//if (n == x) goto END
-	@n
-	D=M
-	@x
-	D=D-M
-	@END
-	D;JEQ
-	// *(R0 + n) = -1
-	@R0
-	D=M
-	@n
-	A=D+M
-	M=-1
-	// n = n + 1
-	@n
-	M=M+1
-	// goto LOOP
-	@LOOP
-	0;JMP
-	
-(END)
-	@END
-	0;JMP
+(LOOPBEGIN)
+@n
+D=M
+@LOOPEND
+D;JEQ
+@counter
+D=M
+@k
+A=D+M
+M=D
+
+@counter
+M=M+1
+
+@n
+M=M-1
+
+
+
+@LOOPBEGIN
+0;JMP
+
+
+
+(LOOPEND)
+
