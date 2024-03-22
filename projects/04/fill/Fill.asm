d712ae342d4b60fa903cdbd1d00fd6f5793dc584
Tanner Steele <tannerjsteele@gmail.com>
2024-03-21 20:38:35 -0500

    Chapter 4 project: Fill.asm finished
    Passes all the tests, but is super inefficient. Ended up taking the explicit route to begin with, and I'll dial back the implementation in a follow up commit.
    
    Elegantly handles overflow and underflow!

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,59 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+
+//CLEAR + BLACKEN SHOULD BE IN DIFFERENT CONDITIONALS
+//could have a color option?
+
+@SCREEN
+D=A
+@SCREENPOS            // holds position of SCREEN in ram
+M=D 
+@COLOR
+M=0
+
+(LOOPSTART)
+	@24575
+	D=M
+	@OVERFLOWDETECTED
+	D;JLT
+	@KBD
+	D=M
+	@KEYPRESSED
+	D;JGT
+	@SCREEN          // if 16384 has a value != 0, allow it to continue
+	D=M
+	@LOOPSTART
+	D;JEQ
+(OVERFLOWDETECTED)
+	@KBD
+	D=M
+	@KEYUNPRESSED    // will only hit here if 16384 is not filled in
+	D;JEQ
+
+
+@LOOPSTART
+0;JMP
+
+//TODO: Redundant, combine logic above
+(KEYPRESSED)
+	@SCREENPOS
+	D=M
+	@COLOR
+	A=D
+	M=-1            //paint it black
+	@SCREENPOS
+	M=D+1
+	@LOOPSTART
+	0;JMP
+
+(KEYUNPRESSED)
+	@SCREENPOS
+	D=M
+	@COLOR
+	A=D
+	M=0            //paint it white
+	@SCREENPOS
+	M=D-1
+	@LOOPSTART
+	0;JMP
